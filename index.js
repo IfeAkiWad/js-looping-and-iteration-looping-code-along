84b32beb9c36fd01ee05a54b1f756a362261c231
github usernameIfeAkiWad <github email addressifeoluwa.wade@gmail.com>
2021-02-17 20:19:51 -0600

    commit tree 0bc60d7cc41ef18bf7b163aff9424e22f5ff0d36

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const names = ['Lisa', 'Kaitlin', 'Jan']
+const event = 'surprise'
+
+function writeCards(names, event) {
+    let msg = []
+    for (let i = 0; i < names.length; i++){
+        
+        msg.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+        // console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    return msg
+}
+
+function countDown(int){
+    // takes in any postive integer, 
+    // from that interger counts down to zero usin console.log()
+    // let int = 11
+    while (int >= 0) {
+        console.log(int)
+        int--
+    }
+    // return int()
+}
